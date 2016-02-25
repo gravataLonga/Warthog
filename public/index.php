afb3f5039b7b160319642fad50c65ab0b43b8b83
jonathan.alexey16@gmail.com <jonathan.alexey16@gmail.com>
2016-02-25 22:10:04 +0000

    Warthog - Http && Boot Service Provider

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Warthog\Warthog;
 
-$app = new Warthog(
-    realpath(__DIR__.'/../')
-);
+$app = new Warthog(realpath(__DIR__.'/../'));
+
+
+$app['slim']->run();
